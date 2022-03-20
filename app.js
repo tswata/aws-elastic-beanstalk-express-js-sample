f6a368a2ffebd2b7569cd672c24b461f4650f5a5
EC2 Default User <ec2-user@ip-172-31-3-117.ec2.internal>
2022-03-20 02:00:39 +0000

    Peace

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Goodevening World!'));
+app.get('/', (req, res) => res.send('Peace World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
